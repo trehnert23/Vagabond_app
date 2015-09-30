498e1f5de2288fcc5d60b1da9c7ec7e2c9d8a8cf
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-30 14:54:36 -0700

    formatting articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,18 @@
 
-User.create(first_name: "steve", last_name: "farquhar", email: "farquhar@gml.com", city: "san francisco", password: "123")
-User.create(first_name: "brad", last_name: "flynn", email: "farquhar@hotmal.com", city: "san francisco", password: "123")
-User.create(first_name: "jon", last_name: "tommy", email: "faar@gmail.com", city: "san francisco", password: "123")
-
-City.create(name: "san francisco")
-City.create(name: "london")
-City.create(name: "gibraltar")
+# City.create(name: "The Moon")
+# City.create(name: "Mars")
+# City.create(name: "Bradley's Sweet Space Palace of Love")
 
 
-Article.create(title: "Air Plane Ride", content: "high in the sky", city: "san francisco", user_id: 1, city_id: 1)
-Article.create(title: "Golden Gate", content: "high in the sky", city: "san francisco", user_id: 1, city_id: 1)
-Article.create(title: "Bay Bridge", content: "high in the sky", city: "san francisco", user_id: 1, city_id: 1)
+User.create(first_name: "steve", last_name: "farquhar", email: "farquhar@gml.com", city: "The Moon", password: "123")
+User.create(first_name: "brad", last_name: "flynn", email: "farquhar@hotmal.com", city: "The Moon", password: "123")
+User.create(first_name: "jon", last_name: "tommy", email: "faar@gmail.com", city: "The Moon", password: "123")
 
 
 
-# User.create(first_name: "steve", last_name: "farquhar", email: "farquhar@gml.com", city: "san francisco", password: "123")
-# User.create(first_name: "brad", last_name: "flynn", email: "farquhar@hotmal.com", city: "san francisco", password: "123")
-# User.create(first_name: "jon", last_name: "tommy", email: "faar@gmail.com", city: "san francisco", password: "123")
 
-# City.create(name: "san francisco")
-# City.create(name: "london")
-# City.create(name: "gibraltar")
-
-
-# Article.create(title: "Air Plane Ride", content: "high in the sky", city: "san francisco", user_id: 1, city_id: 1)
-# Article.create(title: "Golden Gate", content: "high in the sky", city: "san francisco", user_id: 2, city_id: 1)
-# Article.create(title: "Bay Bridge", content: "high in the sky", city: "london", user_id: 3, city_id: 2)
-
-
+# Article.create({title: "Air Plane Ride", content: "high in the sky", city: "The Moon", user_id: 1, city_id: 1}, without_protection: true)
+# Article.create({title: "Golden Gate", content: "high in the sky", city: "The Moon", user_id: 1, city_id: 1}, without_protection: true)
+# Article.create({title: "Bay Bridge", content: "high in the sky", city: "The Moon", user_id: 1, city_id: 1}, without_protection: true)
 
 
